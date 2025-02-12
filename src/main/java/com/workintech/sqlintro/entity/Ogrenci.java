ddf1020485e128df8e5e41ccb7e58816371d306e
nur-ilayda-kaplan <nurilaydakargi@gmail.com>
2025-02-12 11:00:09 +0300

    S17D4 completed

--- src/main/java/com/workintech/sqlintro/entity/Ogrenci.java
+++ src/main/java/com/workintech/sqlintro/entity/Ogrenci.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
+@Component
 @Table(name = "ogrenci")
 public class Ogrenci {
     @Id
